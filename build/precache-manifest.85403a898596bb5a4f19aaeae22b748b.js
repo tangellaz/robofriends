a9a64ecea5f997be8a8dbe728e179b761c466b6d
tangellaz <tangellaz@gmail.com>
2019-10-27 21:28:29 -0700

    robo upload

---
+++ build/precache-manifest.85403a898596bb5a4f19aaeae22b748b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0162355c81881b8a1c79308944d91a3",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "6b6042e3bb267609cb62",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "c0c066aa3138153148a4",
+    "url": "/robofriends/static/css/main.d40f1321.chunk.css"
+  },
+  {
+    "revision": "6b6042e3bb267609cb62",
+    "url": "/robofriends/static/js/2.1e1f07c9.chunk.js"
+  },
+  {
+    "revision": "c0c066aa3138153148a4",
+    "url": "/robofriends/static/js/main.9ac89506.chunk.js"
+  },
+  {
+    "revision": "6af7fd70da35e0253709",
+    "url": "/robofriends/static/js/runtime-main.8790a40a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
